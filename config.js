f22e9e95ca38824454bfabff7b3a58792a854a89
vvieru <vasilevieru@mail.ru>
2017-08-09 16:51:42 +0200

    First commit

---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by vvieru on 09/08/2017.
+ */
+const config = {}
+
+config.pageAccessToken = 'EAAHBCTnweosBAONe0lzs7rqc4pMu7b6wQhhjpnzbftBo1FsJXjmJyXqRPNgOmKnffUC8rhZBrRMcUUpwS2z0mtZBHoqvLsqs034fZCR3wMC8LKYD8YFqxQeaOb7ZB11QTEeO1f6ufRrwguYUUPKgAlEroZBCfgZBZCENI3hQOvXEgZDZD'
+config.validationToken = 'ABCD1234'
+config.recastToken = '3d2e65f662633ce16b2f63a2dfd8512e'
+config.language = 'fr'
+
+module.exports = config
---
+++ src/bot.js
@@ -0,0 +1,50 @@
+import { replyMessage, replyButton } from './facebook.js'
+import config from './../config.js'
+import recastai from 'recastai'
+
+const client = new recastai(config.recastToken)
+
+function handleMessage(event) {
+  const senderID = event.sender.id
+  const messageText = event.message.text
+  const messageAttachments = event.message.attachments
+  if (messageText) {
+    client.request.converseText(messageText, { conversationToken: senderID }).then((res) => {
+      const reply = res.reply()               /* To get the first reply of your bot. */
+      const replies = res.replies             /* An array of all your replies */
+      const action = res.action               /* Get the object action. You can use 'action.done' to trigger a specification action when it's at true. */
+
+      if (!reply) {
+        const options = {
+          messageText: null,
+          buttonTitle: 'My first button',    /* Option of your button. */
+          buttonUrl: 'https://recast.ai/',   /* If you like more option check out ./facebook.js the function replyButton, and look up */
+          buttonType: 'web_url',             /* the facebook doc for button https://developers.facebook.com/docs/messenger-platform/send-api-reference#message */
+          elementsTitle: 'I don\'t get it :(',
+        }
+        replyButton(senderID, options)        /* to reply a button */
+      } else {
+        if (action && action.done === true) {
+          console.log('action is done')
+          // Use external services: use res.memory('notion') if you got a notion from this action
+        }
+        let promise = Promise.resolve()
+        replies.forEach(rep => {
+          promise = promise.then(() => replyMessage(senderID,rep))
+        })
+        promise.then(() => {
+          console.log('ok')
+        }).catch(err => {
+          console.log(err)
+        })
+      }
+    }).catch(err => {
+      console.log(err)
+    })
+  } else if (messageAttachments) {
+    replyMessage(senderID, 'Message with attachment received')
+  }
+}
+module.exports = {
+  handleMessage,
+}
